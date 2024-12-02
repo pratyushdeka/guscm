729d17bb7c550f5db0fafd8aef66d83f918d2d3f
Pratyush Deka <pratyushdeka255@gmail.com>
2024-12-02 12:13:11 +0530

    Initial Commit

---
+++ guscm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  guscm
+//
+//  Created by Pratyush Kumar Deka on 02/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
